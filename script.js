b1365c9537b7050adc392f972cb19fc9ccbbaa1d
FisherSkyi <yu.letian66@gmail.com>
2024-12-29 20:47:28 +0800

    modify index.html and add script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+const newline = "One day you finally knew\nwhat you had to do, and began,";
+console.log(newline);
+
+const myNum2 = 123;
+const myString2 = String(myNum2);
+console.log(typeof myString2);
+let a = prompt("Enter sth")
+console.log(a)
+// string
+
+/*
+One day you flinally knew
+what you had to do, and began,
+*/
